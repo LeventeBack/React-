8262f6ab43d27bf8fcb0278e38d7acb86d7942b7
LeventeBack <backistvanlevente17gmail.com>
2020-08-15 15:28:46 +0300

    Hide API Key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_QUIZ_API_KEY;
   return (
     <div>
       Hello World
+      {API_KEY}
     </div>
   );
 }
