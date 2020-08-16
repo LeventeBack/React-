7daa42c3f639908547c130d6603b6e07bea039a8
LeventeBack <backistvanlevente17gmail.com>
2020-08-16 12:37:42 +0300

    Main menu finished

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect }from 'react'
+import React, { useState, useEffect, useRef }from 'react'
 import './StartMenu.css'
 import axios from 'axios';
 
 function StartMenu() {
     const [categories, setCategories]  = useState([])
+    const difficulityRef = useRef()
+    const categoryRef = useRef()
 
     useEffect(() => {
         axios.get('https://opentdb.com/api_category.php',{
@@ -16,21 +18,22 @@ function StartMenu() {
         }).then(res => {
             setCategories(res.data.trivia_categories)
         })
-
     }, [])
+
+    function handleFormSubmit(e){
+        e.preventDefault()
+    }
 
     return (
         <div className="start-menu">
             <h1>QUIZ GAME</h1>
-            <form onSubmit={e => e.preventDefault()}>
-                <select>
-                    <option selected disabled>Select difficulty</option>
+            <form onSubmit={handleFormSubmit}>
+                <select ref={difficulityRef}>
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>
                 </select>
-                <select>
-                    <option selected disabled>Select category</option>
+                <select ref={categoryRef}>
                     {categories.map(category => (
                         <option key={category.id} value={category.id}>{category.name}</option>
                     ))}
